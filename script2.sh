c57261a2b32ecb2867fd55c9b5e59df5b3d0de68
Gholamreza Fadakar <fadakargholamreza@gmail.com>
2022-06-17 06:22:53 +0000

    fix bug script2.sh remove space

--- script2.sh
+++ script2.sh
@@ -2,7 +2,7 @@
 
 read -p "Enter Number 1:" number1
 read -p "Enter Number 2:" number2
-mul= $[$number1*$number2]
-div= $[$number1/$number2]
+mul=$[$number1*$number2]
+div=$[$number1/$number2]
 echo "zarb = ${mul}, taghsim ${div}"
 
