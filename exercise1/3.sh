37dbbe60ecd801a98c7e523db1da3572640cfafb
Gholamreza Fadakar <fadakargholamreza@gmail.com>
2022-06-17 12:36:11 +0000

    create 3.sh excercise 3

---
+++ exercise1/3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+
+
+FILE=$1
+HOST=$2
+USER=$3
+PD=$4
+
+
+/usr/bin/expect << EOF 
+spawn scp $FILE $USER@$HOST:/home/$USER
+expect "password:"
+send "$PD\r"
+expect "*\r"
+expect "\r"
+EOF
