45addaeafea84acaa63c75c635c2d2d1feab7dce
Gholamreza Fadakar <fadakargholamreza@gmail.com>
2022-06-23 14:16:46 +0000

    complete 3.sh exercise

--- exercise1/3.sh
+++ exercise1/3.sh
@@ -2,16 +2,28 @@
 
 
 
-FILE=$1
-HOST=$2
-USER=$3
-PD=$4
+FILE="/etc/passwd"
+HOST=$1
+USER=$2
+PD=$3
+
+if [ $# -ne 3 ]
+then
+	echo "Usage: script.sh host user password"
+	exit
+fi
 
 
-/usr/bin/expect << EOF 
-spawn scp $FILE $USER@$HOST:/home/$USER
-expect "*password*"
-send "$PD\r"
-expect "*\r"
-expect "\r"
+ping -c 1 -W 3 $HOST > /dev/null
+if [ $? = 0 ]
+then
+	/usr/bin/expect << EOF
+	spawn scp $FILE $USER@$HOST:/home/$USER
+	expect "*password*"
+	send "$PD\r"
+	expect "*\r"
+	expect "\r"
 EOF
+else
+	echo "Server[ $HOST ] not reachable"
+fi
